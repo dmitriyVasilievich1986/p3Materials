8497a2aaa921cc0aaf0e2fa3f960c796db78fda4
dmitriy <dmitriyvasil@gmail.com>
2025-05-01 21:01:17 +0200

    feat: add `DamageMultiplier` constant

---
+++ p3Materials/constants.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class DamageMultiplier(Enum):
+    Reflect = "Reflect"
+    Absorb = "Absorb"
+    Strong = "Strong"
+    Normal = "Normal"
+    Weak = "Weak"
+    Null = "Null"
