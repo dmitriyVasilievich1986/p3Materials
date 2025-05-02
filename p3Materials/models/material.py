192995e79c1a614745eded4f388331c1c0c33dc1
dmitriy <dmitriyvasil@gmail.com>
2025-05-02 12:47:14 +0200

    feat: update `Material` table

--- p3Materials/models/material.py
+++ p3Materials/models/material.py
@@ -1,13 +1,19 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import Mapped, relationship
 
 from p3Materials.models.base import Base
+
+if TYPE_CHECKING:
+    from p3Materials.models.shadow import Shadow
 
 material_shadow_table = Table(
     "material_shadow_table",
     Base.metadata,
     Column("id", Integer, primary_key=True),
-    Column("shadow_id", ForeignKey("shadow.id")),
-    Column("material_id", ForeignKey("material.id")),
+    Column("shadow_id", ForeignKey("shadow.id"), primary_key=True),
+    Column("material_id", ForeignKey("material.id"), primary_key=True),
 )
 
 
@@ -17,3 +23,7 @@ class Material(Base):
     id: int = Column(Integer, primary_key=True)
     name: str = Column(String(255), nullable=False)
     price: int = Column(Integer, nullable=False, default=0)
+
+    shadows: Mapped[list["Shadow"]] = relationship(
+        secondary=material_shadow_table, back_populates="materials"
+    )
