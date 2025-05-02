f8d0f7e2bcd1decb7452f3f7f8aa53019529fcf5
dmitriy <dmitriyvasil@gmail.com>
2025-05-02 15:40:02 +0200

    feat: update main cli Update output text. Make floors optional.

--- p3Materials/__main__.py
+++ p3Materials/__main__.py
@@ -3,6 +3,7 @@ from enum import Enum
 import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import true
 
 from p3Materials.config import SQLALCHEMY_DATABASE_URI
 from p3Materials.models import (
@@ -18,53 +19,26 @@ class Objects(Enum):
     Shadow = "Shadow"
 
 
-def get_shadows(floors: list[int]) -> list[Shadow]:
-    """
-    Get shadows from the database that are on the specified floors.
-
-    :param floors: List of floor numbers to filter shadows by.
-
-    :return: List of Shadow objects.
-    """
-
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
-    session = Session(engine)
-
+def get_shadows(session: Session, floors: tuple[int]) -> list[Shadow]:
     shadows = (
         session.query(Shadow)
         .join(floor_shadow_table, Shadow.id == floor_shadow_table.c.shadow_id)
-        .filter(floor_shadow_table.c.floor_id.in_(floors))
+        .filter(floor_shadow_table.c.floor_id.in_(floors) if floors else true)
         .all()
     )
-
-    session.close()
-    engine.dispose()
     return shadows
 
 
-def get_materials(floors: list[int]) -> list[Material]:
-    """
-    Get materials from the database that are on the specified floors.
-
-    :param floors: List of floor numbers to filter materials by.
-
-    :return: List of Material objects.
-    """
-
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
-    session = Session(engine)
-
+def get_materials(session: Session, floors: tuple[int]) -> list[Material]:
     materials = (
         session.query(Material)
         .join(material_shadow_table, Material.id == material_shadow_table.c.material_id)
         .join(Shadow, Shadow.id == material_shadow_table.c.shadow_id)
         .join(floor_shadow_table, Shadow.id == floor_shadow_table.c.shadow_id)
-        .filter(floor_shadow_table.c.floor_id.in_(floors))
+        .filter(floor_shadow_table.c.floor_id.in_(floors) if floors else true)
         .all()
     )
 
-    session.close()
-    engine.dispose()
     return materials
 
 
@@ -75,21 +49,27 @@ def get_materials(floors: list[int]) -> list[Material]:
     type=click.Choice([str(x) for x in Objects.__members__], case_sensitive=False),
     default=Objects.Shadow.value,
 )
-@click.option("--floor", "-F", type=int, multiple=True, required=True)
-def main(object: str, floor: list[int]):
+@click.option("--floors", "-F", type=int, multiple=True, required=False)
+def main(object: str, floors: tuple[int]):
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    session = Session(engine)
+
     match object:
         case Objects.Shadow.value:
-            shadows = get_shadows(floor)
+            shadows = get_shadows(floors=floors, session=session)
             for shadow in shadows:
-                click.echo(
-                    f"Shadow ID: {shadow.id}, Name: {shadow.name}, Floors: {floor}"
-                )
+                click.echo(str(shadow))
+                for material in shadow.materials:
+                    click.echo(f"\t{material}")
         case Objects.Material.value:
-            materials = get_materials(floor)
+            materials = get_materials(floors=floors, session=session)
             for material in materials:
-                click.echo(
-                    f"Material ID: {material.id}, Name: {material.name}, Price: {material.price}, Floors: {floor}"
-                )
+                click.echo(str(material))
+                for shadow in material.shadows:
+                    click.echo(f"\t{shadow}")
+
+    session.close()
+    engine.dispose()
 
 
 if __name__ == "__main__":
