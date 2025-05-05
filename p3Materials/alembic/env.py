39fe6f15b43fcd00ff339732e29391c670dd0deb
dmitriy <dmitriyvasil@gmail.com>
2025-05-05 22:55:35 +0200

    feat: get db path from config

--- p3Materials/alembic/env.py
+++ p3Materials/alembic/env.py
@@ -3,6 +3,8 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from p3Materials.config import SQLALCHEMY_DATABASE_URI
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,6 +19,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
+
+config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URI)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
