14d67d8aea9f844b173013f689522fa8530f2ab7
dmitriy <dmitriyvasil@gmail.com>
2025-05-06 11:49:01 +0200

    feat: install `react-router`

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import * as React from "react";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
