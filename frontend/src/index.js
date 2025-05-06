15a7421e54ddd641991b84dd4e5a3113ef2a8860
dmitriy <dmitriyvasil@gmail.com>
2025-05-06 12:11:42 +0200

    feat: add index style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import * as React from "react";
 import App from "./App";
+import "./style.scss";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <BrowserRouter>
