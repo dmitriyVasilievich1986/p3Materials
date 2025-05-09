6a85909c5fc6c2642aabafa86edc825472677dd5
dmitriy <dmitriyvasil@gmail.com>
2025-05-09 12:00:37 +0200

    feat: update `App` component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,44 @@
 import * as React from "react";
-import { Navbar } from "./components/navbar";
-import { ShadowsList, ShadowPage } from "./pages/shadow";
+import * as style from "./style.scss";
+
+import { Flex, Layout } from "antd";
 import { Route, Routes } from "react-router";
+import { ShadowForm, ShadowsList } from "./pages/shadow";
+
+import { Breadcrumb } from "./components/breadcrumb";
+import { Navbar } from "./components/navbar";
+
+import classnames from "classnames/bind";
+
+const cx = classnames.bind(style);
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/shadow">
-          <Route index element={<ShadowsList />} />
-          <Route path=":shadowId" element={<ShadowPage />} />
-        </Route>
-      </Routes>
-    </>
+    <Flex gap="middle" wrap>
+      <Layout className={cx("app-container")}>
+        <Header>
+          <Navbar />
+        </Header>
+        <Content className={cx("content-container")}>
+          <Breadcrumb />
+          <div className={cx("content-wrapper")}>
+            <Routes>
+              <Route path="/" element={<div>Home</div>} />
+              <Route path="/shadow">
+                <Route index element={<ShadowsList />} />
+                <Route path="create" element={<ShadowForm />} />
+                <Route path=":shadowId" element={<ShadowForm />} />
+              </Route>
+            </Routes>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center", height: "4rem" }}>
+          persona 3 Materials ©{new Date().getFullYear()} Created by
+          dmitriyvasil@gmail.com
+        </Footer>
+      </Layout>
+    </Flex>
   );
 }
 
