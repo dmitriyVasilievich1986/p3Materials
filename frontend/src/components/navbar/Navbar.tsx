d3ca92948a090d0ec56bf2330ea0d06a815bc3b8
dmitriy <dmitriyvasil@gmail.com>
2025-05-09 11:45:11 +0200

    feat: update `Navbar` component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,40 +1,39 @@
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
+import * as React from "react";
+import * as style from "./style.scss";
+
+import { Flex, Typography } from "antd";
 
 import { NavLink } from "react-router";
-import * as React from "react";
+import { PagesUrls } from "../../constants";
 
 import classnames from "classnames/bind";
-import * as style from "./style.scss";
 
 const cx = classnames.bind(style);
 
 function Navbar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Container maxWidth="md">
-          <Box className={cx("navbar-container")}>
-            <NavLink to="/" className={(isActive) => cx("navlink", isActive)}>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Home
-              </Typography>
-            </NavLink>
+    <Flex className={cx("navbar-container")} justify="center">
+      <Flex
+        className={cx("navbar-wrapper")}
+        justify="start"
+        align="center"
+        gap="large"
+      >
+        {Object.values(PagesUrls)
+          .filter((p) => !!p.isNavigation)
+          .map((p) => (
             <NavLink
-              to="/shadow"
-              className={(isActive) => cx("navlink", isActive)}
+              to={p.url}
+              key={p.name}
+              className={(isActive) => cx("navbar-link", isActive)}
             >
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Shadow
-              </Typography>
+              <Typography.Title level={4} style={{ color: "inherit" }}>
+                {p.label}
+              </Typography.Title>
             </NavLink>
-          </Box>
-        </Container>
-      </Toolbar>
-    </AppBar>
+          ))}
+      </Flex>
+    </Flex>
   );
 }
 
