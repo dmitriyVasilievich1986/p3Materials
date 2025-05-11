d301b0c85e2cd74ff1bd97eb70b98705e7653787
dmitriy <dmitriyvasil@gmail.com>
2025-05-11 10:05:04 +0200

    feat: return defaulr create_app

--- p3Materials/cli/main.py
+++ p3Materials/cli/main.py
@@ -1,19 +1,5 @@
-from flask import Flask
 from flask.cli import FlaskGroup
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 
-from p3Materials.config import MIGRATIONS_DIR, SQLALCHEMY_DATABASE_URI
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-
-    db = SQLAlchemy(app)
-    Migrate(app, db, directory=MIGRATIONS_DIR)
-
-    return app
-
+from p3Materials.app import create_app
 
 main = FlaskGroup(create_app=create_app)
